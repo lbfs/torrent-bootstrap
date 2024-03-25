5f45a49f21d30ba6f850516ac7460714ccce86c7
Luke Barr <lukebarr@outlook.com>
2024-03-25 04:49:10 +0000

    Add CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{path::{Path, PathBuf}, time::Instant};
+use std::{fs::File, io::Read, path::{Path, PathBuf}, time::Instant};
 
-use crate::orchestrator::{OrchestratorOptions, Orchestrator};
+use crate::{orchestrator::{Orchestrator, OrchestratorOptions}, torrent::Torrent};
+use clap::Parser;
 
 mod finder;
 mod torrent;
@@ -9,15 +10,58 @@ mod bencode;
 mod writer;
 mod solver;
 
-fn main() {
+#[derive(Parser)] // requires `derive` feature
+#[command(version, about, long_about = None)]
+struct Cli {
+    // Path that should be used to load a torrent
+    #[arg(long, required = true, num_args = 1..)]
+    torrents: Vec<String>,
+
+    // Absolute path that should be scanned to find identical pieces
+    #[arg(long, required = true, num_args = 1..)]
+    scan: Vec<String>,
+
+    // Absolute path where the merged or updated file should be placed.
+    #[arg(long, required = true)]
+    export: String,
+
+    // Number of threads to perform scanning and hashing.
+    #[arg(long, required = true)]
+    threads: usize,
+}
+
+fn main() -> std::io::Result<()> {
     let now = Instant::now();
 
+    let args = Cli::parse();
+
     // Load Torrents
-    let torrents = vec![];
+    let mut torrents: Vec<Torrent> = Vec::new();
+    for torrent_path_as_string in &args.torrents {
+        let torrent_as_path = Path::new(torrent_path_as_string);
+
+        if !(torrent_as_path.exists() && torrent_as_path.is_file()) {
+            Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Export directory does not exist or is not a directory."))?
+        }
+
+        let mut handle = File::open(torrent_as_path)?;
+        let mut buffer = Vec::new();
+        handle.read_to_end(&mut buffer)?;
+
+        let torrent = Torrent::from_bytes(&buffer)
+            .map_err(|err| std::io::Error::new(std::io::ErrorKind::InvalidData, err.message))?;
+
+        torrents.push(torrent);
+    }
     let torrent_len = torrents.len();
 
-    let scan_directories: Vec<PathBuf> = Vec::new();
-    let export_directory = Path::new(r#""#).to_path_buf();
+
+    let mut scan_directories: Vec<PathBuf> = Vec::new();
+    for scan_directory in &args.scan {
+        scan_directories.push(Path::new(scan_directory).to_path_buf());
+    }
+
+    let export_directory = Path::new(&args.export).to_path_buf();
 
     let options = OrchestratorOptions {
         torrents: torrents,
@@ -32,4 +76,6 @@ fn main() {
 
     let elapsed = now.elapsed().as_secs();
     println!("Time elapsed took {} seconds for {} torrents.", elapsed, torrent_len);
+
+    Ok(())
 }
